25bc3312fc162f009ab1e441e52ccdcdf21fe18e
Verity Collins <verity.carlos@gmail.com>
2023-03-23 16:00:37 -0500

    added code to app

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom'
+import Plants from "./Plants";
+import { UserProvider } from "../context/user";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import Plant from "./Plant"
+import EditPlant from "./EditPlant";
+import PlantForm from "./PlantForm";
+
+function App() {
+  return (
+    <div className="App">
+      <UserProvider>
+        <NavBar />
+        <br/>
+        <br/>
+        <Routes>
+          <Route exact path="/" element = {<Home />} />
+          <Route exact path="/plants" element = {<Plants />} />
+          <Route exact path="/plants/:id" element = {<Plant />} />
+          <Route exact path="/plants/:id/edit" element = {<EditPlant />} />
+          <Route exact path="/plants/new" element = {<PlantForm />} />
+          <Route exact path="/signup" element = {<SignUp />} />
+          <Route exact path="/login" element = {<LogIn />} />
+        </Routes>
+      </UserProvider>
+    </div>
+  );
+}
+
+export default App;
